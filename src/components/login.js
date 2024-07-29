625559e1b45611b5f18b76d3a7afb3b9c2b0d9fd
Zack Honey <zhoney466@gmail.com>
2024-07-28 17:06:37 -0700

    Loginpage info

--- src/components/login.js
+++ src/components/login.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Login = () => {
-  return <>Login Page</>;
+  return <>
+    <h1>Login Page</h1>
+    <p>
+      Customers information will be stored here.
+      <br/>
+      Need to create a db to store users and so information can be stored and pulled from
+    </p>
+  </>;
 };
 
 export default Login;
